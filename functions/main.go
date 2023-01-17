59d0dd3cc540c807998717582645c8da1c7130c7
hodeiez <hodeinaiz@hotmail.com>
2023-01-17 23:28:33 +0100

    asadad

--- functions/main.go
+++ functions/main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	// "hodei/web1/db"
+	"context"
 
-	"hodei/web1/controller"
-	envs "hodei/web1/env"
-	gui "hodei/web1/mygui"
-	"os"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 )
 
-func main() {
-	//test db
-	if len(os.Args) > 1 && os.Args[1] == envs.Get("RUN_LOCAL") {
-		println("running local setup")
-		gui.Init()
-	} else {
-		controller.InitApi()
-		println("running api")
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
 	}
-	// albumKey := "ngshzau1u2nk"
-	// infoCardKey := "bkf482hx7vmx"
-	// trackKey := "23lob0l15zk8"
-	// jason, _ := json.Marshal(service.GetAlbumDTOByKey(albumKey))
-	// fmt.Println(string(jason))
-	// infojson, _ := json.MarshalIndent(service.GetInfoCardDTOByKey(infoCardKey), "", "   ")
-	// fmt.Println(string(infojson))
-	// os.WriteFile("file.mp3", service.GetAudioFileByKey(trackKey), os.ModeDevice.Perm())
-	// infoCardByType, _ := json.MarshalIndent(db.GetInfoCardsByType(db.Creative), "", "   ")
-	// fmt.Println(string(infoCardByType))
+
+	cc := lc.ClientContext
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello, " + cc.Client.AppTitle,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
 }
